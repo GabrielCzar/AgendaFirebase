f9df2c62506ade405247aa3d5f55496984cc4c4a
Gabriel Cesar <gcal1@live.com>
2017-11-23 04:06:22 -0300

    config initial files

---
+++ app/src/main/java/com/agenda/models/Contact.java
@@ -0,0 +1,21 @@
+package com.agenda.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Contact {
+    public String uid;
+    public String name;
+    public String email;
+    public String phone;
+
+    public Contact() {}
+
+    public Contact(String uid, String name, String email, String phone) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+}
